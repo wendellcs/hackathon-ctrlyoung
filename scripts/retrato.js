9f2f6ebdf90d1cd22a5434c937078bf178b8dbf6
bernardcsilva <bercarvalheira.silva@gmail.com>
2025-04-29 16:38:40 -0300

    Menu Hámburguer

--- scripts/retrato.js
+++ scripts/retrato.js
@@ -1,6 +1,13 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
+const menuToggle = document.getElementById('menu-toggle')
+const fecharMenu = document.querySelector('.menu-X')
+const headerLinks = document.querySelector('.header-links')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+fecharMenu.addEventListener('click', () => {
+  mobileMenu.style.display = 'none'
+})
 
 menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+    mobileMenu.style.display = 'block'
 });
+
